fd07f870391f90f1175e1784a7bc756d626d5aad
Javicorporation <krchipullajavier@gmail.com>
2024-06-02 23:05:35 -0500

    implementamos alerta con opcion multiple

--- componentes/src/app/pages/alert/alert.page.ts
+++ componentes/src/app/pages/alert/alert.page.ts
@@ -7,6 +7,8 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./alert.page.scss'],
 })
 export class AlertPage implements OnInit {
+  handlerMessage ='';
+  roleMessage = '';
 
   constructor(private alertCtrl: AlertController) { }
 
@@ -17,6 +19,7 @@ export class AlertPage implements OnInit {
     this.presentarAlert();
   }
 
+  // boton de alerta de una opcion
   async presentarAlert(){
     const alert = await this.alertCtrl.create({
       header:'Alert',
@@ -26,5 +29,27 @@ export class AlertPage implements OnInit {
     });  
     await alert.present();  
   }
+
+  // boton de alerta de multiple opcion
+  async presentarMultipleAlert(){
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: [{
+        text: 'OK!',
+        handler: () => {
+        console.log('Click en OK!')
+        }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'rojo'
+          }]
+    });
+    await alert.present();
+  };
 
 }
